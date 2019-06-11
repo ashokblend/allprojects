d12c43a391179d83aa4db30ff0dc5b48686732a0
ashok.blend <ashok.blend@gmail.com>
2019-06-11 12:56:00 +0530

    python thread

---
+++ python/PythonCross/com/ashok/py/test/PythonThread.py
@@ -0,0 +1,27 @@
+'''
+Created on 11-Jun-2019
+
+@author: ashok.kumar
+'''
+
+from thread import start_new_thread
+
+def heron(a):
+    """Calculates the square root of a"""
+    eps = 0.0000001
+    old = 1
+    new = 1
+    while True:
+        old,new = new, (new + a/new) / 2.0
+        #print old, new
+        if abs(new - old) < eps:
+            break
+    print(new)
+    return new
+
+start_new_thread(heron,(99,))
+start_new_thread(heron,(999,))
+start_new_thread(heron,(1733,))
+
+
+c = raw_input("Type something to quit.")
